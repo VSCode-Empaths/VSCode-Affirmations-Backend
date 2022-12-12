bde6de95f98f44a4dd55664a78b999ae38472496
Rio Edwards <rioredwards@gmail.com>
2022-12-12 12:30:53 -0800

    Add Get Affirmation by id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-// Dummy user for testing
 const mockAffirmation = {
   text: 'If all else fails, I can collapse and scream 🦆',
   category_id: '4',
@@ -54,6 +53,30 @@ describe('affirmations and category routes', () => {
       affirmations: expect.any(Array),
     });
   });
+
+  it('GET /api/v1/affirmations/:id should get a single affirmation', async () => {
+    const insertAffirmationRes = await request(app)
+      .post('/api/v1/affirmations')
+      .send(mockAffirmation);
+    const res = await request(app).get(
+      `/api/v1/affirmations/${insertAffirmationRes.body.id}`
+    );
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      created_at: expect.any(String),
+      ...mockAffirmation,
+    });
+  });
+
+  /* it('DELETE /api/v1/affirmations/:id should delete an affirmation', async () => {
+    const affirmation = await affirmation.insert(mockAffirmation);
+    const resp = await app.delete(`/api/v1/affirmations/${affirmation.id}`);
+    expect(resp.status).toBe(200);
+
+    const check = await affirmation.getById(affirmation.id);
+    expect(check).toBeNull();
+  }); */
 
   afterAll(() => {
     pool.end();
