dd98e4fc90762677adb28d6cec0927f009c8b5fd
kevinnail <fumalicious@gmail.com>
2022-12-12 11:11:25 -0800

    affirmation route added, test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET api/v1/affirmations should return a list of affirmations', async () => {
+    const res = await request(app).get('/affirmations');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      text: expect.any(String),
+      category_id: expect.any(String),
+      created_at: expect.any(String),
+    });
   });
   afterAll(() => {
     pool.end();
