157b5a05577917740df7275f78a042ced4218ffd
Rio Edwards <rioredwards@gmail.com>
2022-12-12 12:12:57 -0800

    Add Create Affirmation route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,12 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+// Dummy user for testing
+const mockAffirmation = {
+  text: 'If all else fails, I can collapse and scream 🦆',
+  category_id: '4',
+};
+
 describe('affirmations and category routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -17,6 +23,19 @@ describe('affirmations and category routes', () => {
       created_at: expect.any(String),
     });
   });
+
+  it('POST api/v1/affirmations should create a new affirmation', async () => {
+    const res = await request(app)
+      .post('/api/v1/affirmations')
+      .send(mockAffirmation);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      created_at: expect.any(String),
+      ...mockAffirmation,
+    });
+  });
+
   it('GET api/v1/categories should return a list of categories', async () => {
     const res = await request(app).get('/api/v1/categories');
     expect(res.status).toBe(200);
